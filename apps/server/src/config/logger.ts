a0aa8abc63415dfc97a932181c171ffea0e67abb
daniellx42 <daniellx42.job@gmail.com>
2026-03-16 15:50:05 -0300

    bugfix: logger not compile in production

--- apps/server/src/config/logger.ts
+++ apps/server/src/config/logger.ts
@@ -1,18 +1,27 @@
 import { logger } from "@bogeychan/elysia-logger";
 import { Elysia } from "elysia";
 
+const isProduction = process.env.NODE_ENV === "production";
+const isCompiledBinary = !process.execPath.includes("bun");
+
+const usePrettyLogger = !isProduction && !isCompiledBinary;
+
 export const loggerPino = new Elysia().use(
   logger({
-    level: "debug",
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        translateTime: "HH:MM:ss.l",
-        ignore: "pid,hostname",
-        messageFormat: "{request.method} {request.url} - {responseTime}ms",
-        singleLine: true,
-      },
-    },
+    level: isProduction ? "info" : "debug",
+    ...(usePrettyLogger
+      ? {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "HH:MM:ss.l",
+            ignore: "pid,hostname",
+            messageFormat: "{request.method} {request.url} - {responseTime}ms",
+            singleLine: true,
+          },
+        },
+      }
+      : {}),
   })
 );
